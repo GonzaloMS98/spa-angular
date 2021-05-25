0d4fec2e5c902eec7de07ac26dcf3fc0972968a3
GonzaloMS98 <gonzalomonsa94@gmail.com>
2021-05-24 20:20:36 -0500

    main commit

---
+++ src/app/app.routing.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core'
+import {Routes,RouterModule} from '@angular/router'
+import {HeroesComponent} from './components/heroes/heroes.component'
+import {HomeComponent} from './components/home/home.component'
+import {AboutComponent} from './components/about/about.component'
+
+const routes : Routes  = [
+    {path:'home',component:HomeComponent},
+    {path:'heroes',component:HeroesComponent},
+    {path:'about', component:AboutComponent},
+    {path:'**', pathMatch:'full',redirectTo:'home'}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
